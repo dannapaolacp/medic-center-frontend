a414b9cc53ad184bd9af20408dfe6ac9627f40ae
Danna Paola <dannacapera16@gmail.com>
2022-12-03 23:59:46 -0500

    Se realiza la mitad de las validaciones..

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,58 +1,205 @@
 $('#registrationbutton').click(() => {
-  let registrationPrueba = {
-    cc: 123456789,
-    name: 'Marl',
-    password: 'Marl',
-    phone: '63',
-    email: 'a@gmail.com',
-    age: 40,
-    weight: 64,
-    height: 1.7,
-    birth: '2000-01-10',
-    // answer: answer.value,
-    // response: response.value,
-  };
+  if (cc.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su cedula',
+    });
+    cc.value = '';
+    cc.focus();
+    return false;
+  } else if (cc.value <= 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su cedula correctamente',
+    });
+    cc.value = '';
+    cc.focus();
+    return false;
+  }
+  if (names.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su nombre',
+    });
+    names.value = '';
+    names.focus();
+    return false;
+  }
+  if (password.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su contraseña',
+    });
+    password.value = '';
+    password.focus();
+    return false;
+  }
+  if (passwordConfirm.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su comprobación de contraseña',
+    });
+    passwordConfirm.value = '';
+    passwordConfirm.focus();
+    return false;
+  }
+  if (age.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su edad',
+    });
+    age.value = '';
+    age.focus();
+    return false;
+  } else if (age.value <= 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su edad correctamente',
+    });
+    age.value = '';
+    age.focus();
+    return false;
+  }
+  if (phone.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su telefono',
+    });
+    phone.value = '';
+    phone.focus();
+    return false;
+  } else if (phone.value <= 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su edad correctamente',
+    });
+    phone.value = '';
+    phone.focus();
+    return false;
+  }
+  if (email.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su correo',
+    });
+    email.value = '';
+    email.focus();
+    return false;
+  }
+  if (weight.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su peso',
+    });
+    weight.value = '';
+    weight.focus();
+    return false;
+  } else if (weight.value <= 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su edad correctamente',
+    });
+    weight.value = '';
+    weight.focus();
+    return false;
+  }
 
-  console.table(registrationPrueba);
-  let registrationJson = JSON.stringify(registrationPrueba);
-  console.log(registrationJson);
+  if (height.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su estatura',
+    });
+    height.value = '';
+    height.focus();
+    return false;
+  } else if (height.value <= 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su edad correctamente',
+    });
+    height.value = '';
+    height.focus();
+    return false;
+  }
 
-  fetch('http://localhost:3000/api/patients', {
-    method: 'Post',
-    body: registrationJson,
-  });
+  if (birth.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su fecha de nacimiento',
+    });
+    birth.value = '';
+    birth.focus();
+    return false;
+  }
 
-  // if (password.value === passwordConfirm.value) {
-  //   let registration = {
-  //     cc: cc.value,
-  //     name: names.value,
-  //     password: password.value,
-  //     phone: phone.value,
-  //     email: email.value,
-  //     age: age.value,
-  //     weight: weight.value,
-  //     height: height.value,
-  //     birth: birth.value,
-  //     // answer: answer.value,
-  //     // response: response.value,
-  //   };
+  if (answer.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe escoger una pregunta',
+    });
+    answer.value = '';
+    answer.focus();
+    return false;
+  }
 
-  //   console.table(registration);
-  //   let registrationJson = JSON.stringify(registration);
-  //   console.log(registrationJson);
+  if (response.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su respuesta de seguridad',
+    });
+    response.value = '';
+    response.focus();
+    return false;
+  }
 
-  //   fetch('http://localhost:3000/api/patients', {
-  //     method: 'Post',
-  //     body: registrationJson,
-  //   });
-  // } else {
-  //   Swal.fire({
-  //     icon: 'error',
-  //     title: 'ERROR!!',
-  //     text: 'La contraseña es diferente',
-  //   });
-  //   passwordConfirm.value = '';
-  //   passwordConfirm.focus();
-  //   return false;
-  // }
+  if (password.value === passwordConfirm.value) {
+    let registration = {
+      cc: cc.value,
+      name: names.value,
+      password: password.value,
+      phone: phone.value,
+      email: email.value,
+      age: age.value,
+      weight: weight.value,
+      height: height.value,
+      birth: birth.value,
+      answer: answer.value,
+      response: response.value,
+    };
+
+    console.table(registration);
+    let registrationJson = JSON.stringify(registration);
+    console.log(registrationJson);
+
+    fetch('http://localhost:3000/patients', {
+      method: 'Post',
+      body: registrationJson,
+    });
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'La contraseña es diferente',
+    });
+    passwordConfirm.value = '';
+    passwordConfirm.focus();
+    return false;
+  }
 });
