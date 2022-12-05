3b98840218305ffe10c8766e0353a22e8acbbb1b
Danna Paola <dannacapera16@gmail.com>
2022-12-04 20:19:45 -0500

    Se realiza el mostrar doctores

--- public/pacientes/assets/js/script.js
+++ public/pacientes/assets/js/script.js
@@ -2,7 +2,22 @@ function tabladoctores() {
   fetch('http://localhost:3000/api/medics')
     .then((response) => response.json())
     .then((doctors) => {
-      console.log(doctors);
+      let tablaDoctors = document.querySelector('#table-doctors tbody');
+
+      for (const itemDoctors of doctors) {
+        let tr =
+          '<tr> <td>' +
+          itemDoctors.name +
+          '</td><td>' +
+          itemDoctors.phone +
+          '</td><td>' +
+          itemDoctors.email +
+          '</td><td>' +
+          itemDoctors.speciality +
+          '</td></tr>';
+
+        tablaDoctors.innerHTML += tr;
+      }
     });
 }
 
