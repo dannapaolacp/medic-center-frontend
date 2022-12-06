511ef9c1d178ac3d08ae1e3cf1777c1da59e70cf
gianfranco <frangot301@gmail.com>
2022-12-05 23:15:52 -0500

    Se desarrollan las validaciones del crear doctor, faltando subirlo adecuadamente a la base de datos

---
+++ public/administrador/assets/js/functionscrear.js
@@ -0,0 +1,191 @@
+let buttonEnviar = document.getElementById('registrationbutton');
+
+buttonEnviar.addEventListener('click', function (event) {
+  event.preventDefault();
+  let form = document.getElementById('registrationform');
+  let cc = document.getElementById('cc');
+  let names = document.getElementById('names');
+  let email = document.getElementById('email');
+  let phone = document.getElementById('phone');
+  let speciality = document.getElementById('speciality');
+  let password = document.getElementById('password');
+  let passwordConfirm = document.getElementById('passwordConfirm');
+  let answer = document.getElementById('answer');
+  let response = document.getElementById('response');
+
+  if (cc.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su cedula',
+    });
+    cc.value = '';
+    cc.focus();
+    return false;
+  } else if (cc.value <= 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su número de tarjeta profesional correctamente',
+    });
+    cc.value = '';
+    cc.focus();
+    return false;
+  }
+  if (names.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su nombre',
+    });
+    names.value = '';
+    names.focus();
+    return false;
+  }
+  const patternn = new RegExp('^[A-Z ]+$', 'i');
+  if (!patternn.test(names.value)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'No debe tener numeros ni caracteres especiales en el nombre',
+    });
+    names.value = '';
+    names.focus();
+    return false;
+  }
+  if (email.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su correo',
+    });
+    email.value = '';
+    email.focus();
+    return false;
+  }
+  if (phone.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su telefono',
+    });
+    phone.value = '';
+    phone.focus();
+    return false;
+  } else if (phone.value <= 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su edad correctamente',
+    });
+    phone.value = '';
+    phone.focus();
+    return false;
+  }
+  if (speciality.value == 'Seleccione') {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Seleccione una pregunta adecuada',
+    });
+    speciality.value = '';
+    speciality.focus();
+    return false;
+  } else if (speciality.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Seleccione una pregunta adecuada',
+    });
+    speciality.value = '';
+    speciality.focus();
+    return false;
+  }
+  if (password.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su contraseña',
+    });
+    password.value = '';
+    password.focus();
+    return false;
+  }
+  if (passwordConfirm.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su comprobación de contraseña',
+    });
+    passwordConfirm.value = '';
+    passwordConfirm.focus();
+    return false;
+  }
+  if (answer.value == 'Seleccione la pregunta de seguridad') {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe escoger una pregunta',
+    });
+    answer.value = '';
+    answer.focus();
+    return false;
+  } else if (answer.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe escoger una pregunta',
+    });
+    answer.value = '';
+    answer.focus();
+    return false;
+  }
+  if (response.value == 0) {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'Debe insertar su respuesta de seguridad',
+    });
+    response.value = '';
+    response.focus();
+    return false;
+  }
+  if (password.value === passwordConfirm.value) {
+    let registration = {
+      cc: cc.value,
+      name: names.value,
+      password: password.value,
+      phone: phone.value,
+      email: email.value,
+      answer: answer.value,
+      response: response.value,
+    };
+
+    console.table(registration);
+    let registrationJson = JSON.stringify(registration);
+    console.log(registrationJson);
+
+    fetch('http://localhost:3000/api/patients', {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: registrationJson,
+    });
+    Swal.fire({
+      icon: 'success',
+      title: 'Se ha creado tu usuario correctamente!!',
+    }).then(function () {
+      window.location = '/administrador';
+    });
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR!!',
+      text: 'La contraseña es diferente',
+    });
+    passwordConfirm.value = '';
+    passwordConfirm.focus();
+    return false;
+  }
+});
--- public/administrador/assets/js/scripts.js
+++ public/administrador/assets/js/scripts.js
@@ -1,16 +1,30 @@
 let buttonDeleteMedics = document.getElementById('eliminar');
 let buttonDeletePacients = document.getElementById('eliminar');
-buttonDeleteMedics.addEventListener('click', function () {
+buttonDeleteMedics.addEventListener('click', function (event) {
+  event.preventDefault();
   let cc = document.getElementById('professionalCard');
   fetch(`http://localhost:3000/api/medic/${cc.value}`, {
     method: 'DELETE',
   });
+  Swal.fire({
+    icon: 'success',
+    title: 'Se ha eliminado el doctor correctamente!!',
+  }).then(function () {
+    window.location = '/administrador';
+  });
 });
 
-buttonDeletePacients.addEventListener('click', function () {
+buttonDeletePacients.addEventListener('click', function (event) {
+  event.preventDefault();
   let cc = document.getElementById('cc');
   console.log(cc.value);
   fetch(`http://localhost:3000/api/patient/${cc.value}`, {
     method: 'DELETE',
+  });
+  Swal.fire({
+    icon: 'success',
+    title: 'Se ha eliminado el paciente correctamente!!',
+  }).then(function () {
+    window.location = '/administrador';
   });
 });
