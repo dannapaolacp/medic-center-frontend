86c697bc90e9fef03b91078062398db1d3311479
Marlon H <marlon7hrr@gmail.com>
2022-12-17 20:59:26 -0500

    Se modifico calendario de citas en pacientes: se quito tipo de cita y en su lugar se agrego especialidad y se creo la tabla para mostrar datos de cita, en este mismo apartado, se agregaron las acciones de confirmar y modificar cita.... Se agrego la pagina de consultar historia clinica en medicos...

--- public/pacientes/assets/js/scriptViewDate.js
+++ public/pacientes/assets/js/scriptViewDate.js
@@ -3,7 +3,20 @@ function tableViewDate() {
       .then((response) => response.json())
       .then((viewDate) => {
         let tableViewDate = document.querySelector('#table-viewDate tbody');        
-
+        let dirM = "'modifyAppointment.html'";          
+        let iconoM = '<button class="mt-3" style="right: 50px; border: 0;" id="actualizar"><img style="width: 30px; height: 30px;"'                        
+        + 'onclick="location.href='+ dirM +'"'
+        + 'src="assets/img/lapiz.png"'
+        + 'alt="cita"'
+        + 'title="editar"'
+        + '/></button> '; 
+        let dirC = "'modifyAppointment.html'";          
+        let iconoC = '<button class="mt-3" style="right: 50px; border: 0;" id="actualizar"><img style="width: 30px; height: 30px;"'                        
+        + 'onclick="location.href='+ dirC +'"'
+        + 'src="assets/img/confirmar.png"'
+        + 'alt="cita"'
+        + 'title="confirmar"'
+        + '/></button> ';
         for (const itemViewDate of viewDate) {
           let tr =
             '<tr> <td>' +
@@ -11,12 +24,16 @@ function tableViewDate() {
             '</td><td>' +
             itemViewDate.hour +
             '</td><td>' +
-            itemViewDate.speciality +
+            itemViewDate.date+
             '</td><td>' +
-            itemViewDate.doctor +
+            itemViewDate.specialty +
             '</td></tr>' +            
+            itemViewDate.doctor +
+            '</td></tr>' +
             itemViewDate.id_visit_status +
-            '</td></tr>';
+            '</td></tr>' +
+            iconoM + iconoC +
+            '</td></tr>';             
   
           tableViewDate.innerHTML += tr;
         }
